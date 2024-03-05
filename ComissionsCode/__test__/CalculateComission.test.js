d690b4a87df2c6781405a17151b61f6089975f16
System Administrator <root@macbook-2.local>
2024-03-05 11:44:53 -0500

    Test file added

---
+++ ComissionsCode/__test__/CalculateComission.test.js
@@ -0,0 +1,11 @@
+import { calculateCommissionFunction } from '../CalculateCommission';
+
+describe('calculateCommissionFunction', () => {
+    test('commission for total sales less than or equal to 1000', 
+	() => {
+const [totalSales, commission] = calculateCommissionFunction(10, 10, 10);
+
+        expect(totalSales).toBe(1000);
+        expect(commission).toBe(100);
+    });
+
