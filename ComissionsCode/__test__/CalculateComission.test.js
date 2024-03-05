3310a306ad50f22079e3a0d0e77646d51832b5bf
regloff11 <158495317+regloff11@users.noreply.github.com>
2024-03-05 16:34:58 -0500

    Update CalculateComission.test.js

--- ComissionsCode/__test__/CalculateComission.test.js
+++ ComissionsCode/__test__/CalculateComission.test.js
@@ -1,11 +1,18 @@
+// In CalculateCommission.js
+
+export function calculateCommissionFunction(sales, rate, base) {
+    // implementation
+}
+
+// In CalculateCommission.test.js
+
 import { calculateCommissionFunction } from '../CalculateCommission';
 
 describe('calculateCommissionFunction', () => {
-    test('commission for total sales less than or equal to 1000', 
-	() => {
-const [totalSales, commission] = calculateCommissionFunction(10, 10, 10);
+    test('commission for total sales less than or equal to 1000', () => {
+        const [totalSales, commission] = calculateCommissionFunction(10, 10, 10);
 
         expect(totalSales).toBe(1000);
         expect(commission).toBe(100);
     });
-
+});
